ed2eb4f1ec7bd8c0ae15cd9349b89453189c97f3
Anand Patel <anandpat4@gmail.com>
2015-11-23 17:11:42 +0530

    ssss

--- src/main/java/com/anand/studentApp/controllers/HomeController.java
+++ src/main/java/com/anand/studentApp/controllers/HomeController.java
@@ -42,7 +42,7 @@ public class HomeController {
 		 * 
 		 * String formattedDate = dateFormat.format(date);
 		 * 
-		 * model.addAttribute("serverTime", formattedDate);
+		 *      model.addAttribute("serverTime", formattedDate);
 		 */
 
 		return "home";
